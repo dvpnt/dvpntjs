a4444946fc79d018c16d233bc744bbe8f5a9ef67
Aleksey Timchenko <flegmatik.bel@gmail.com>
2017-08-21 16:37:53 +0300

    add collection

---
+++ src/helpers/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _ = require('underscore');
+
+_(exports).extend(
+	require('./query')
+);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+exports.Collection = require('./utils/Collection');
+exports.helpers = require('./helpers');
